cd584ec9144a9ec20f38cd71f0f031da27b046c5
ZimConnect <berryzcarwash@gmail.com>
2026-07-30 16:16:13 +0200

    Update package name and simplify MainActivity class

--- android/app/src/main/kotlin/com/zimconnect/shofe_rider/MainActivity.kt
+++ android/app/src/main/kotlin/com/zimconnect/shofe_rider/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.shofe.rider
+package com.shofe.rider // CHANGE THIS TO YOUR ACTUAL PACKAGE NAME
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
