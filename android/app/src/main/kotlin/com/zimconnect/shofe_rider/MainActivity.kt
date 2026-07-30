24f3458fa7cfb41848a3025bafc2ca5c25485182
ZimConnect <berryzcarwash@gmail.com>
2026-07-30 10:02:30 +0200

    Refactor MainActivity to extend FlutterActivity

--- android/app/src/main/kotlin/com/zimconnect/shofe_rider/MainActivity.kt
+++ android/app/src/main/kotlin/com/zimconnect/shofe_rider/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.shoferider.shofe_rider
+package com.shofe.rider
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
 }
